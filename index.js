be35f4a923e7bef94db3ed329e3571f8c50ec27e
nomayen31 <nomayen.ohin@gmail.com>
2025-09-30 08:21:58 +0600

    new task post ans store to the DB

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8k7klrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const ClusterConnect = client.db("ClusterConnect")
+    const ClusterConnectCollection = ClusterConnect.collection("taskess")
+
+    app.post('/add-task', async(req, res)=>{
+        const NewTask = req.body;
+        console.log('NewTask',NewTask);
+        const result = await ClusterConnectCollection.insertOne(NewTask)
+        res.send(result)
+        
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Cluster Connect Server Home page");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Cluster Connect server is running on port ${port}`);
+  });
+  
